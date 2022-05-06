105f645a997d3959e88d8198059b1b13ebd63679
Aniket Kumar Roy <aniketroy769@gmail.com>
2022-05-06 15:47:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,28 +18,63 @@ def extract(img):
     conts,_=cv.findContours(img,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
     for cnts in conts:
         peri=cv.arcLength(cnts,True)
-        approx=cv.approxPolyDP(cnts,0.04*peri,True)
+        approx=cv.approxPolyDP(cnts,0.05*peri,True)
         A,B,C,D=approx[approx[:,:,1].argmin()][0],approx[approx[:,:,0].argmax()][0],approx[approx[:,:,1].argmax()][0],approx[approx[:,:,0].argmin()][0]
         if fabs(dist(A,B)-dist(B,C))<0.04*peri and fabs(dist(B,C)-dist(C,D))<0.04*peri and fabs(dist(C,D)-dist(D,A))<0.04*peri and fabs(dist(A,C)-dist(B,D))<0.04*peri:
             cv.fillPoly(mask,[approx],(255))
     mask=cv.erode(mask,(7,7),iterations=3)
     return (mask,img2)
 def redraw(img):
-    blank=np.zeros(img.shape,np.uint8)
-    v=[[[85,131,80],[136,255,255]],[[37,140,100],[94,255,255]],[[171,150,140],[179,255,255]],
-    [[0,131,128],[5,255,255]],[[0,0,175],[179,25,255]],[[6,131,170],[20,255,255]],[[21,115,160],[36,255,255]],
-    [[37,95,195],[40,140,255]]]
-    c=[(255,0,0),(0,255,0),(0,0,255),(0,0,255),(255,255,255),(0,145,255),(0,255,255),(0,255,255)]
+    IMAGE = np.zeros(img.shape,np.uint8)
+    v=[[[83,131,80],[136,255,255]],                       #blue
+    [[37,140,100],[82,255,255]],                          #green
+    [[171,150,140],[179,255,255],[0,131,128],[5,255,255]],#red
+    [[0,0,175],[179,25,255]],                             #white
+    [[6,131,170],[23,255,255]],                           #orange
+    [[24,115,160],[37,255,255],[38,95,195],[40,140,255]]] #yellow
+
+    c=[(255,0,0), # blue
+    (0,255,0),    #green
+    (0,0,255),    #red
+    (255,255,255),#white
+    (0,145,255),  #orange
+    (0,255,255)]  #yellow
+
+    imghsv=cv.cvtColor(img,cv.COLOR_BGR2HSV)
     for i,j in enumerate(v):
-        imghsv=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+        blank=np.zeros(img.shape[:-1],np.uint8)
         lower=np.array(j[0])
         upper=np.array(j[1])
         mask=cv.inRange(imghsv,lower,upper)
+        if len(j) == 4:
+            lower=np.array(j[2])
+            upper=np.array(j[3])
+            mask2=cv.inRange(imghsv,lower,upper)
+            mask = cv.add(mask,mask2)
         mask=cv.dilate(mask,(7,7),iterations=5)
         cont,_=cv.findContours(mask,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
-        for cnt in cont:
-            cv.fillPoly(blank,[cnt],c[i])
-    return blank  
+        for cnts in cont:
+            cv.fillPoly(blank,[cnts],(255))
+        # IMG = np.zeros(img.shape,np.uint8)
+        # IMG[:,:,0],IMG[:,:,1],IMG[:,:,2] = c[i][0],c[i][1],c[i][2]
+        # cv.imshow("blank",blank)
+        # cv.imshow("colour",IMG)
+        # cv.imshow("original",img)
+        # cv.waitKey(1000)
+        cont,_=cv.findContours(blank,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+        if len(cont) != 0:
+            max_area = max([cv.contourArea(c) for c in cont])
+        for cnts in cont:
+            peri=cv.arcLength(cnts,True)
+            approx=cv.approxPolyDP(cnts,0.05*peri,True)
+            # A,B,C,D=approx[approx[:,:,1].argmin()][0],approx[approx[:,:,0].argmax()][0],approx[approx[:,:,1].argmax()][0],approx[approx[:,:,0].argmin()][0]
+            # if fabs(dist(A,B)-dist(B,C))<0.1*peri and fabs(dist(B,C)-dist(C,D))<0.1*peri and fabs(dist(C,D)-dist(D,A))<0.1*peri and fabs(dist(A,C)-dist(B,D))<0.1*peri:
+            if len(approx)==4 and cv.contourArea(approx) > 0.6*max_area:
+                cv.fillPoly(IMAGE,[cnts],c[i])
+            # cv.imshow("original",img)
+            # cv.imshow("out",IMAGE)
+            # cv.waitKey(1000)
+    return IMAGE
 def align(img):
     h,w=img.shape[0:-1]
     arr=np.array([[w//2,h//2]])
@@ -54,17 +89,17 @@ def align(img):
     mat=cv.getPerspectiveTransform(pt1,pt2)
     img2=cv.warpPerspective(img,mat,(w,h))
     return img2
-
-img=cv.imread("image10.jpg")
-mask,_=extract(img)
-out=cv.bitwise_and(img,img,mask=mask)
-out=cv.fastNlMeansDenoisingColored(out,None, 30,9,6, 21)
-out=cv.bilateralFilter(out,15,75,75)
-img2=redraw(out)
-img3=align(img2)
-# cv.imshow("original",img)
-# cv.imshow("output",out)  
-# cv.imshow("redrawn",img2)
-# cv.imshow("align",img3)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+for i in [1,2,3,8,9,10]:
+    img=cv.imread(f"input//cube{i}.jpeg")
+    mask,_=extract(img)
+    out=cv.bitwise_and(img,img,mask=mask)
+    out=cv.fastNlMeansDenoisingColored(out,None, 30,9,6, 21)
+    out=cv.bilateralFilter(out,15,75,75)
+    img2=redraw(out)
+    img3=align(img2)
+    # cv.imshow("original",img)
+    # cv.imshow("output",mask)  
+    cv.imshow("redrawn",img2)
+    # cv.imshow("align",img3)
+    cv.waitKey(200)
+cv.destroyAllWindows()
